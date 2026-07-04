589a7eba5afd2acf9fc866737858f6b5d77b2b37
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-04 11:48:07 +0530

    small improvement

--- checkpalindrome.c
+++ checkpalindrome.c
@@ -1,34 +1,21 @@
 #include<stdio.h>
 
-int main () {
+int main() {
 
-    int n = 1214;
+    int n = 121;
+    int original = n;
+    int reversed = 0;
 
-    int dupn = n;
-
-    int reverse = 0;
-
-    while(dupn > 0){
-
-        int lastdigit = dupn % 10;
-
-        reverse = reverse * 10 + lastdigit;
-
-        dupn /= 10;
+    while (n > 0) {
+        int digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n /= 10;
     }
-    
-   if (n == reverse) {
 
-        printf("%d is palindrome Number",n);
-
-   }
-
-   else {
-
-        printf("%d is not a palindrome Number",n);
-
-   }
+    if (original == reversed)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
 
     return 0;
-
 }
